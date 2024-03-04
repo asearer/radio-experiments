cbdf92c41482ca6cbc647e68f2650fe6416e2562
asearer <amsearer84@gmail.com>
2024-03-04 12:45:34 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   trilateration/trilateration.py added visualization

--- trilateration/trilateration.py
+++ trilateration/trilateration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def trilaterate(beacons, distances):
     """
@@ -18,9 +19,42 @@ def trilaterate(beacons, distances):
     y = solution[1]
     return x, y
 
+def plot_trilateration(beacons, distances, target_location):
+    """
+    Plot the beacons, target location, and circles representing distances.
+    
+    Args:
+    - beacons (list of tuples): Coordinates of beacons in the format [(x1, y1), (x2, y2), ...].
+    - distances (list of floats): Distances from the target to each beacon.
+    - target_location (tuple): Estimated coordinates (x, y) of the target.
+    """
+    plt.figure()
+    
+    # Plot beacons
+    for beacon in beacons:
+        plt.plot(beacon[0], beacon[1], 'bo')
+    
+    # Plot target location
+    plt.plot(target_location[0], target_location[1], 'ro', label='Estimated Target Location')
+    
+    # Plot circles representing distances
+    for i in range(len(beacons)):
+        circle = plt.Circle(beacons[i], distances[i], color='gray', fill=False, linestyle='dotted')
+        plt.gca().add_patch(circle)
+    
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Trilateration')
+    plt.axis('equal')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 # Example usage:
 beacons = [(0, 0), (3, 0), (0, 4)]  # Coordinates of the beacons
 distances = [5, 4, 3]  # Distances from the target to each beacon
 
 target_location = trilaterate(beacons, distances)
 print("Estimated target location:", target_location)
+
+plot_trilateration(beacons, distances, target_location)

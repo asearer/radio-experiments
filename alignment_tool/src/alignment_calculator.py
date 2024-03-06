4504448d9e6d783e2cbe2ae1c9338ab0f1bf2f08
asearer <amsearer84@gmail.com>
2024-03-06 10:16:18 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	deleted:    alignment_tool/run.py 	deleted:    alignment_tool/src/__init__.py 	deleted:    alignment_tool/src/alignment_calculator.py 	deleted:    alignment_tool/src/gui.py

--- alignment_tool/src/alignment_calculator.py
+++
@@ -1,7 +0,0 @@
-class AlignmentCalculator:
-    @staticmethod
-    def calculate_alignment(satellite_azimuth, satellite_elevation, antenna_azimuth, antenna_elevation):
-        # Example calculation - replace with actual calculation
-        alignment_azimuth = satellite_azimuth - antenna_azimuth
-        alignment_elevation = satellite_elevation - antenna_elevation
-        return alignment_azimuth, alignment_elevation
